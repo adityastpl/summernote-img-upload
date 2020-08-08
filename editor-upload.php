d180c1bea8ebab853d7773ff7c0ee14a123a2030
Aditya Yadav <yadavaditya0@gmail.com>
2020-08-08 18:10:50 +0530

    upload file fix

--- editor-upload.php
+++ editor-upload.php
@@ -1,14 +1,13 @@
 <?php
-if(empty($_FILES['file']))
-{
-	exit();
-}
+
 $errorImgFile = "./img/img_upload_error.jpg";
-$destinationFilePath = './img-uploads/'.$_FILES['file']['name'];
-if(!move_uploaded_file($_FILES['file']['tmp_name'], $destinationFilePath)){
+if(!empty($_FILES['file'])) {
+	$destinationFilePath = './img-uploads/'.$_FILES['file']['name'];
+	if(!move_uploaded_file($_FILES['file']['tmp_name'], $destinationFilePath)) {
+		echo $errorImgFile;
+	} else {
+		echo $destinationFilePath;
+	}
+} else {
 	echo $errorImgFile;
 }
-else{
-	echo $destinationFilePath;
-}
-?>
